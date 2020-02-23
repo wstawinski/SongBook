2e3cc0e51973ec208fdca4c23f022ac31613edc7
Wojciech <stawinskiwojciech@gmail.com>
2020-02-23 19:59:51 +0100

    Fixed Performer GetEmpty

--- SongBook.API/Controllers/PerformerController.cs
+++ SongBook.API/Controllers/PerformerController.cs
@@ -2,7 +2,6 @@
 using SongBook.API.Controllers.Base;
 using SongBook.Domain.Interfaces.Manager;
 using SongBook.Domain.Models;
-using System.Collections.Generic;
 
 namespace SongBook.API.Controllers
 {
@@ -16,34 +15,7 @@ namespace SongBook.API.Controllers
         [HttpGet("GetEmpty")]
         public Performer GetEmpty()
         {
-            return new Performer
-            {
-                Songs = new List<Song>
-                {
-                    new Song
-                    {
-                        Paragraphs = new List<Paragraph>
-                        {
-                            new Paragraph
-                            {
-                                Lines = new List<Line>
-                                {
-                                    new Line
-                                    {
-                                        LineChords = new List<LineChord>
-                                        {
-                                            new LineChord
-                                            {
-                                                Chord = new Chord()
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            };
+            return new Performer();
         }
     }
 }
